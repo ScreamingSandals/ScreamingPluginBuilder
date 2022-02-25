956050f8e074ab1a5541e9ba6124e3d74bc1ad2a
František Novosad <frantisek@nvsd.dev>
2022-02-25 22:35:32 +0100

    [1.0.75] Do releasing after evaluate

--- builder/src/main/groovy/org/screamingsandals/gradle/builder/BuilderPlugin.groovy
+++ builder/src/main/groovy/org/screamingsandals/gradle/builder/BuilderPlugin.groovy
@@ -79,151 +79,6 @@ class BuilderPlugin implements Plugin<Project> {
             }
         }
 
-        if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
-            project.task('sourceJar', type: Jar) {
-                it.classifier 'sources'
-                from project.sourceSets.main.allJava
-            }
-        }
-
-        if (System.getenv('JAVADOC_HOST') != null) {
-            def srcmain = project.file("src/main");
-
-            project.javadoc {
-                options.addBooleanOption('html5', true)
-            }
-
-            project.task('javadocJar', type: Jar, dependsOn: project.javadoc) {
-                it.classifier = 'javadoc'
-                from project.javadoc
-            }
-
-            if (System.getenv('JAVADOC_HOST') != null && System.getenv('JAVADOC_USER') != null && System.getenv('JAVADOC_SECRET') != null) {
-                project.tasks.register("uploadJavadoc") {
-                    doLast {
-                        try {
-                            def projectJavadocDirectories
-                            def custom = System.getProperty("JavadocUploadCustomDirectoryPath")
-                            if (custom != null && !custom.isEmpty()) {
-                                if (project.getRootProject() == project) {
-                                    projectJavadocDirectories = [custom]
-                                } else {
-                                    projectJavadocDirectories = [custom, project.getName()]
-                                }
-                            } else {
-                                if (project.getRootProject() == project) {
-                                    projectJavadocDirectories = [project.getName()]
-                                } else {
-                                    projectJavadocDirectories = [project.getRootProject().getName(), project.getName()]
-                                }
-                            }
-
-                            def jsch = new JSch()
-                            def jschSession = jsch.getSession(System.getenv('JAVADOC_USER'), System.getenv('JAVADOC_HOST'))
-                            jschSession.setConfig("StrictHostKeyChecking", "no");
-                            jschSession.setPassword(System.getenv('JAVADOC_SECRET'))
-                            jschSession.connect()
-                            def sftpChannel = jschSession.openChannel("sftp") as ChannelSftp
-                            sftpChannel.connect()
-
-                            sftpChannel.cd("www")
-
-                            projectJavadocDirectories.forEach {
-                                try {
-                                    sftpChannel.cd(it)
-                                } catch (SftpException ignored) {
-                                    sftpChannel.mkdir(it)
-                                    sftpChannel.cd(it)
-                                }
-                            }
-
-                            recursiveClear(sftpChannel)
-
-                            recursiveFolderUpload(sftpChannel, project.file('build/docs/javadoc'))
-
-                            sftpChannel.disconnect()
-
-                            jschSession.disconnect()
-                        } catch (SftpException exception) {
-                            exception.printStackTrace()
-                        }
-                    }
-                    dependsOn('javadoc')
-                }
-            }
-        }
-
-        PublishingExtension publishing = project.extensions.getByName("publishing")
-        def maven = publishing.publications.create("maven", MavenPublication) {
-            it.artifact(project.tasks.jar)
-
-            it.artifacts.every {
-                it.classifier = ""
-            }
-
-            if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
-                it.artifact(project.tasks.sourceJar)
-            }
-
-            it.pom.withXml {
-                def dependenciesNode = asNode().appendNode("dependencies")
-                project.configurations.compileOnly.dependencies.each {
-                    if (!(it instanceof SelfResolvingDependency)) {
-                        def dependencyNode = dependenciesNode.appendNode('dependency')
-                        dependencyNode.appendNode('groupId', it.group)
-                        dependencyNode.appendNode('artifactId', it.name)
-                        dependencyNode.appendNode('version', it.version)
-                        dependencyNode.appendNode('scope', 'provided')
-                    }
-                }
-                if (!project.tasks.findByName("shadowJar")) {
-                    project.configurations.api.dependencies.each {
-                        def dependencyNode = dependenciesNode.appendNode('dependency')
-                        dependencyNode.appendNode('groupId', it.group)
-                        dependencyNode.appendNode('artifactId', it.name)
-                        dependencyNode.appendNode('version', it.version)
-                        dependencyNode.appendNode('scope', 'compile')
-                    }
-                }
-            }
-        }
-
-        project.ext['enableShadowPlugin'] = {
-            project.apply {
-                plugin ShadowPlugin.class
-            }
-
-            project.tasks.getByName("screamCompile").dependsOn -= "build"
-            project.tasks.getByName("screamCompile").dependsOn += "shadowJar"
-
-            maven.each {
-                it.getArtifacts().removeIf {
-                    it.buildDependencies.getDependencies().contains(project.tasks.jar)
-                }
-                it.artifact(project.tasks.shadowJar) {
-                    it.classifier = ""
-                }
-            }
-        }
-
-        if (System.getenv("GITLAB_REPO") != null) {
-            new GitlabRepository().setup(project, publishing)
-        }
-
-        if (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null) {
-            new NexusRepository().setup(project, publishing)
-        }
-
-        List tasks = ["build"]
-
-        if (!ciCdOptimized) {
-            tasks.add("publishToMavenLocal")
-        }
-
-        if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
-            tasks.add("publish")
-        }
-
         project.getExtensions().create("discord", DiscordWebhookExtension)
 
         project.tasks.create("screamCompile").dependsOn = tasks
@@ -231,6 +86,153 @@ class BuilderPlugin implements Plugin<Project> {
         project.tasks.create("allowJavadocUpload") {
             if (project.tasks.findByName("uploadJavadoc") != null) {
                 dependsOn("uploadJavadoc")
+            }
+        }
+
+        project.afterEvaluate {
+            if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
+                project.task('sourceJar', type: Jar) {
+                    it.classifier 'sources'
+                    from project.sourceSets.main.allJava
+                }
+            }
+
+            if (System.getenv('JAVADOC_HOST') != null) {
+                def srcmain = project.file("src/main");
+
+                project.javadoc {
+                    options.addBooleanOption('html5', true)
+                }
+
+                project.task('javadocJar', type: Jar, dependsOn: project.javadoc) {
+                    it.classifier = 'javadoc'
+                    from project.javadoc
+                }
+
+                if (System.getenv('JAVADOC_HOST') != null && System.getenv('JAVADOC_USER') != null && System.getenv('JAVADOC_SECRET') != null) {
+                    project.tasks.register("uploadJavadoc") {
+                        doLast {
+                            try {
+                                def projectJavadocDirectories
+                                def custom = System.getProperty("JavadocUploadCustomDirectoryPath")
+                                if (custom != null && !custom.isEmpty()) {
+                                    if (project.getRootProject() == project) {
+                                        projectJavadocDirectories = [custom]
+                                    } else {
+                                        projectJavadocDirectories = [custom, project.getName()]
+                                    }
+                                } else {
+                                    if (project.getRootProject() == project) {
+                                        projectJavadocDirectories = [project.getName()]
+                                    } else {
+                                        projectJavadocDirectories = [project.getRootProject().getName(), project.getName()]
+                                    }
+                                }
+
+                                def jsch = new JSch()
+                                def jschSession = jsch.getSession(System.getenv('JAVADOC_USER'), System.getenv('JAVADOC_HOST'))
+                                jschSession.setConfig("StrictHostKeyChecking", "no");
+                                jschSession.setPassword(System.getenv('JAVADOC_SECRET'))
+                                jschSession.connect()
+                                def sftpChannel = jschSession.openChannel("sftp") as ChannelSftp
+                                sftpChannel.connect()
+
+                                sftpChannel.cd("www")
+
+                                projectJavadocDirectories.forEach {
+                                    try {
+                                        sftpChannel.cd(it)
+                                    } catch (SftpException ignored) {
+                                        sftpChannel.mkdir(it)
+                                        sftpChannel.cd(it)
+                                    }
+                                }
+
+                                recursiveClear(sftpChannel)
+
+                                recursiveFolderUpload(sftpChannel, project.file('build/docs/javadoc'))
+
+                                sftpChannel.disconnect()
+
+                                jschSession.disconnect()
+                            } catch (SftpException exception) {
+                                exception.printStackTrace()
+                            }
+                        }
+                        dependsOn('javadoc')
+                    }
+                }
+            }
+
+            PublishingExtension publishing = project.extensions.getByName("publishing")
+            def maven = publishing.publications.create("maven", MavenPublication) {
+                it.artifact(project.tasks.jar)
+
+                it.artifacts.every {
+                    it.classifier = ""
+                }
+
+                if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
+                    it.artifact(project.tasks.sourceJar)
+                }
+
+                it.pom.withXml {
+                    def dependenciesNode = asNode().appendNode("dependencies")
+                    project.configurations.compileOnly.dependencies.each {
+                        if (!(it instanceof SelfResolvingDependency)) {
+                            def dependencyNode = dependenciesNode.appendNode('dependency')
+                            dependencyNode.appendNode('groupId', it.group)
+                            dependencyNode.appendNode('artifactId', it.name)
+                            dependencyNode.appendNode('version', it.version)
+                            dependencyNode.appendNode('scope', 'provided')
+                        }
+                    }
+                    if (!project.tasks.findByName("shadowJar")) {
+                        project.configurations.api.dependencies.each {
+                            def dependencyNode = dependenciesNode.appendNode('dependency')
+                            dependencyNode.appendNode('groupId', it.group)
+                            dependencyNode.appendNode('artifactId', it.name)
+                            dependencyNode.appendNode('version', it.version)
+                            dependencyNode.appendNode('scope', 'compile')
+                        }
+                    }
+                }
+            }
+
+            project.ext['enableShadowPlugin'] = {
+                project.apply {
+                    plugin ShadowPlugin.class
+                }
+
+                project.tasks.getByName("screamCompile").dependsOn -= "build"
+                project.tasks.getByName("screamCompile").dependsOn += "shadowJar"
+
+                maven.each {
+                    it.getArtifacts().removeIf {
+                        it.buildDependencies.getDependencies().contains(project.tasks.jar)
+                    }
+                    it.artifact(project.tasks.shadowJar) {
+                        it.classifier = ""
+                    }
+                }
+            }
+
+            if (System.getenv("GITLAB_REPO") != null) {
+                new GitlabRepository().setup(project, publishing)
+            }
+
+            if (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null) {
+                new NexusRepository().setup(project, publishing)
+            }
+
+            List tasks = ["build"]
+
+            if (!ciCdOptimized) {
+                tasks.add("publishToMavenLocal")
+            }
+
+            if (System.getenv("GITLAB_REPO") != null || (System.getenv("NEXUS_URL_SNAPSHOT") != null && System.getenv("NEXUS_URL_RELEASE") != null)) {
+                tasks.add("publish")
             }
         }
     }

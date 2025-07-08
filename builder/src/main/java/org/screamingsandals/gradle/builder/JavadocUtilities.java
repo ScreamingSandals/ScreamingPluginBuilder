6e9c2869584caf4b0b69482d82f152c1f003865d
Misat11 <20199703+Misat11@users.noreply.github.com>
2025-07-08 19:28:20 +0200

    feat(builder): apiNote and other javadoc tags

--- builder/src/main/java/org/screamingsandals/gradle/builder/JavadocUtilities.java
+++ builder/src/main/java/org/screamingsandals/gradle/builder/JavadocUtilities.java
@@ -18,9 +18,12 @@ package org.screamingsandals.gradle.builder;
 
 import org.gradle.api.Project;
 import org.gradle.api.tasks.bundling.Jar;
+import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.api.tasks.javadoc.Javadoc;
 import org.gradle.external.javadoc.CoreJavadocOptions;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public final class JavadocUtilities {
     private JavadocUtilities() {
@@ -34,6 +37,14 @@ public final class JavadocUtilities {
             var javadoc = (Javadoc) javadocTask;
             javadoc.options(op -> {
                 ((CoreJavadocOptions) op).addBooleanOption("html5", true);
+
+                var javac = project.getTasks().withType(JavaCompile.class).getByName("compileJava");
+
+                var release = javac.getOptions().getRelease();
+                if (release.isPresent()) {
+                    ((CoreJavadocOptions) op).addStringOption("-release", release.get().toString());
+                }
+                ((CoreJavadocOptions) op).addMultilineStringsOption("tag").setValue(List.of("apiNote:a:API Note:", "implSpec:a:Implementation Requirements:", "implNote:a:Implementation Note:"));
             });
             if (useSourcesJarAsInput) {
                 var sourcesJarTask = project.getTasks().withType(Jar.class).getByName(Constants.SOURCES_JAR_TASK_NAME);

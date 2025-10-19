a74be263043973dfe2613b88cb9227a9ffad00bb
Misat11 <20199703+Misat11@users.noreply.github.com>
2025-10-19 14:38:55 +0200

    fix: fix license check

--- slib/src/main/kotlin/org/screamingsandals/gradle/slib/SLibSingleModule.kt
+++ slib/src/main/kotlin/org/screamingsandals/gradle/slib/SLibSingleModule.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.screamingsandals.gradle.slib
 
 import org.gradle.api.artifacts.dsl.DependencyHandler
